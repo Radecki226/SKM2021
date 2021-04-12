7608f50d0e039d19fde44bb651ecd9a8aa2b491f
radecki226 <radecki226@gmail.com>
2021-04-12 14:52:04 +0200

    CLI0

---
+++ CLI/cli.c
@@ -0,0 +1,80 @@
+/*INCLUDES*/
+#include "cli.h"
+#include "main.h"
+#include "cstring"
+/****************************************************GLOBAL VARIABLES*******************************************************************************/
+char* ip = "192.168.1.1";
+/**************************************************DEFINITIONS from cli.h header********************************************************************/
+void ReceiveBuffer_Init(ReceiveBuffer *receiveBuffer, char *dataBuffer, size_t dataBufferSize ){
+	receiveBuffer->buffer = dataBuffer;
+	receiveBuffer->capacity = dataBufferSize;
+	receiveBuffer->count = 0;
+	receiveBuffer->full = false;
+}
+void ReceiveBuffer_Clear(ReceiveBuffer *receiveBuffer){
+	
+	for(size_t i=0; i < receiveBuffer->count; i++)
+		receiveBuffer->buffer[i] = 0;
+	receiveBuffer->count = 0;
+	receiveBuffer->full = false;
+			
+}
+bool ReceiveBuffer_PutChar(ReceiveBuffer *receiveBuffer, char c){
+	if(receiveBuffer->full == true)
+		return false;
+	receiveBuffer->buffer[receiveBuffer->count] = c;
+	receiveBuffer->count++;
+	if(receiveBuffer->count == receiveBuffer->capacity)
+		receiveBuffer->full = true;
+	return true;
+}
+bool ReceiveBuffer_Backspace(ReceiveBuffer *receiveBuffer){
+	if(receiveBuffer->count == 0)
+		return false;
+	receiveBuffer->buffer[receiveBuffer->count-1] = 0;
+	receiveBuffer->count--;
+	if(receiveBuffer->count < receiveBuffer->capacity)
+		receiveBuffer->full = false;
+	return true;
+}
+
+void CLI_ExecuteCommand(char* data,UART_HandleTypeDef huart){
+		
+		uint8_t terminator[] = {'\r','\n',0};
+		char *unrecognizedCommand = "Unrecongnized Command";
+		
+		if(strcmp(data,"ip") == 0){
+		HAL_UART_Transmit(&huart, (uint8_t*)ip, strlen(ip), 10);
+		HAL_UART_Transmit(&huart,terminator,3,10);
+	}
+	else{
+		HAL_UART_Transmit(&huart, (uint8_t*)unrecognizedCommand, strlen(unrecognizedCommand), 10);
+		HAL_UART_Transmit(&huart,terminator,3,10);		
+	}
+}
+
+
+void CLI_CommandLine(uint8_t* myRxData,ReceiveBuffer *receiveBuffer,UART_HandleTypeDef huart){
+	
+	uint8_t currentChar = myRxData[0];
+	uint8_t terminator[] = {'\r','\n',0};
+
+	switch(currentChar){
+		case 127:
+			if(ReceiveBuffer_Backspace(receiveBuffer) == true)
+				HAL_UART_Transmit(&huart ,myRxData, 1, 10);
+			break;
+		case '\r':
+			HAL_UART_Transmit(&huart,terminator,3,10);
+			CLI_ExecuteCommand(receiveBuffer->buffer, huart);
+			HAL_UART_Transmit(&huart,terminator,3,10);
+			ReceiveBuffer_Clear(receiveBuffer);
+			break;
+		default:
+			if(ReceiveBuffer_PutChar(receiveBuffer, currentChar) == true)
+					HAL_UART_Transmit(&huart, myRxData, 1, 10);
+			break;
+	}
+}
+
+
---
+++ CLI/main.c
@@ -0,0 +1,190 @@
+#include "main.h"
+#include "cli.h"
+#include "cstring"
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART2_UART_Init(void);
+
+
+
+
+/*global variables*/
+uint8_t myRxData[1];
+ReceiveBuffer receiveBuffer;
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
+
+
+
+
+/*Interrupt's callback*/
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  UNUSED(huart);
+	CLI_CommandLine(myRxData,&receiveBuffer,huart2);
+}
+
+
+
+
+
+int main(void)
+{
+
+  HAL_Init();
+
+
+  SystemClock_Config();
+
+
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+	HAL_UART_Receive_DMA(&huart2, myRxData, 1);
+	
+	char data[20];
+	size_t dataSize = sizeof(data);
+	ReceiveBuffer_Init(&receiveBuffer,data,dataSize);
+	
+
+	
+	
+	
+
+  while (1)
+  {
+	
+  }
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel6_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel6_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel6_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
