54f0803da295830caeb9cd9b98c5af10285e24ca
jestemlidzia <lidkaa.wu@gmail.com>
2021-04-20 13:24:11 +0200

    monitor

--- CLI/cli.h
+++ CLI/cli.h
@@ -1,13 +1,13 @@
 /**
- *This header contains the API for CLI application 
+ *This header contains the API for CLI application
  *
- *The project has benn prepared by P.Radecki, A.Król and L.Wisniewska.
+ *The project has benn prepared by P.Radecki, A.KrÃ³l and L.Wisniewska.
  *The project aim is to implement CLI to communicate between PC and STM32 MCU
  *
  */
 /*INCLUDES*/
 #include "stdbool.h"
-#include "stm32f3xx_hal.h"
+#include "stm32l0xx_hal.h"
 
 /**
  *Structure defining receive buffer
@@ -16,7 +16,7 @@
  *
  *->buffer is char* array containing the data
  *->capacity indicates maximal number of chars stored in the buffer
- *->count is current number of chars in the buffer, it also indicates the address of the next write 
+ *->count is current number of chars in the buffer, it also indicates the address of the next write
  *->full flag indicates whether the buffer is full, if full == true it is impossible to write to buffer
 */
 typedef struct{
@@ -25,6 +25,20 @@ typedef struct{
 	int count;
 	bool full;
 }ReceiveBuffer;
+
+
+
+typedef struct {
+	int counter;
+	char *array;
+	size_t size;
+}Monitor;
+
+
+
+
+
+
 
 /*Initialize buffer with array of particular size*/
 void ReceiveBuffer_Init(ReceiveBuffer *receiveBuffer, char *dataBuffer, size_t dataBufferSize );
@@ -39,9 +53,16 @@ bool ReceiveBuffer_PutChar(ReceiveBuffer *receiveBuffer, char c);
 bool ReceiveBuffer_Backspace(ReceiveBuffer *receiveBuffer);
 
 /*Executes command according to data string and reply to uart*/
-void CLI_ExecuteCommand(char* data,UART_HandleTypeDef huart);
-	
+void CLI_ExecuteCommand(char* data,UART_HandleTypeDef huart, Monitor *monitor);
+
 /*API for command line, this function defines interface*/
-void CLI_CommandLine(uint8_t* myRxData,ReceiveBuffer *receiveBuffer,UART_HandleTypeDef huart);
+void CLI_CommandLine(uint8_t* myRxData,ReceiveBuffer *receiveBuffer,UART_HandleTypeDef huart, Monitor *monitor);
 
+/*Function initializing monitor*/
+void Monitor_Init(Monitor *monitor, char* data, size_t dataSize);
 
+/*Function responsible for adding data to monitor*/
+bool Monitor_Add(Monitor *monitor, char* data, size_t dataSize);
+
+/*Function responsible for clearing monitor array*/
+void Monitor_Clear(Monitor *monitor);
